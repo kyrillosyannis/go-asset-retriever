5d2409806093c2782e6a33e2aafd9d50f8ec0e36
yanniskyrillos <yannis.kyrillos@athenarc.gr>
2025-03-11 13:07:43 +0200

    add main files

--- src/main.go
+++ src/main.go
@@ -5,7 +5,9 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
+	database "go.challenge/config"
 	"go.challenge/controller"
+	"go.challenge/repository"
 	"go.challenge/service"
 )
 
@@ -18,16 +20,23 @@ func main() {
 	if err != nil { // Handle errors reading the config file
 		panic(fmt.Errorf("fatal error config file: %w", err))
 	}
+
+	db, err := database.ConnectDatabase()
+	if err != nil {
+		fmt.Printf("Failed to connect to database: %v", err)
+	}
 	// Initialize Gin
 	r := gin.Default()
 
 	// Initialize service and controller
-	chartService := &service.ChartService{}
+	chartRepository := repository.NewChartRepository(db)
+	chartService := service.NewChartService(chartRepository)
 	chartController := controller.NewChartController(chartService)
 
 	// Define endpoint
 	r.GET("/message", chartController.GetMessageHandler)
+	r.GET("/charts", chartController.GetAllCharts)
 
 	// Start server
-	r.Run(":8081") // Runs on http://localhost:8080
+	r.Run(":8081") // Runs on http://localhost:8081
 }
